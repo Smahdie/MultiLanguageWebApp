cefd83f0c7cc40708ab0f43968206657bbcb1687
Mahdieh Hosseini <m.hosseini@farakav.com>
2021-06-25 19:16:33 +0430

    support models

---
+++ MultiLanguageWebsite/Models/ContactRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MultiLanguageWebsite.Models
+{
+    public class ContactRequest
+    {
+        [Display(Name = "Name")]
+        [MaxLength(50)]
+        [RegularExpression(@"[\u0600-\u06FFa-zA-Z\s]+", ErrorMessage = "Use only Persian or English letters.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [MaxLength(11)]
+        [RegularExpression("[(0-9)(۰-۹)]*", ErrorMessage = "{0} must contain maximum 11 digits.")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Message text")]
+        [Required(ErrorMessage = "{0} is required")]
+        [RegularExpression(@"(\n)*[-_\u0600-\u06FFa-zA-Z0-9۰-۹\s]+(\n|-_\u0600-\u06FFa-zA-Z0-9۰-۹\s)*", ErrorMessage = "You have not entered {0} or it contains forbidden characters.")]
+        [MaxLength(200)]
+        public string Message { get; set; }
+    }
+}
--- MultiLanguageWebsite/Startup.cs
+++ MultiLanguageWebsite/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Routing;
@@ -23,9 +22,10 @@ namespace MultiLanguageWebsite
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
-        {
+        {            
             services.Configure<RequestLocalizationOptions>(options =>
             {
+                //Part 1: support English and Persian languages
                 var supportedCultures = new[] {
                     new CultureInfo("fa"),
                     new CultureInfo("en")
@@ -34,25 +34,28 @@ namespace MultiLanguageWebsite
                 options.SupportedCultures = supportedCultures;
                 options.SupportedUICultures = supportedCultures;
 
+                // Part 2: introduce a way to detect current culture
                 var provider = new RouteValueRequestCultureProvider { Options = options };
-
                 options.RequestCultureProviders = new List<IRequestCultureProvider>
                 {
                     provider
                 };
             });
 
+            //Part 3: Tell the app to look for resource files in the **Resources** folder
             services.AddLocalization(options => options.ResourcesPath = "Resources");
 
+            //Part 4: Add a constraint to routeOptions, to check the culture with **CultureRouteConstraint** class
             services.Configure<RouteOptions>(options =>
             {
                 options.ConstraintMap.Add("culture", typeof(CultureRouteConstraint));
             });
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
             services.AddMvc()
+                //Part 5: Support localization in View files
                 .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
                 .AddDataAnnotationsLocalization();
+
             //other configurations here
         }
 
@@ -62,10 +65,6 @@ namespace MultiLanguageWebsite
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
 
             app.UseStaticFiles();
 
@@ -73,11 +72,13 @@ namespace MultiLanguageWebsite
 
             app.UseAuthorization();
 
+            //Part 6
             var options = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
             app.UseRequestLocalization(options.Value);
 
             app.UseEndpoints(endpoints =>
             {
+                //Part 7: introduce a routing that supports culture
                 endpoints.MapControllerRoute(
                      name: "default",
                     pattern: "{culture}/{controller=Home}/{action=Index}/{id?}",
